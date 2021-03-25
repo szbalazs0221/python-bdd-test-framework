1bf0f0796dfad2a5a696885189a7f94bb84aa8b1
baszabo <balazs.szabo@ni.com>
2021-03-25 08:42:58 +0100

    refactoring done

--- pages/loginpage.py
+++ pages/loginpage.py
@@ -7,6 +7,7 @@ class LoginPage:
     email_field = (By.CSS_SELECTOR, "#user_email")
     password_field = (By.CSS_SELECTOR, "#user_password")
     sign_in_button = (By.CSS_SELECTOR, "input[value=\"Sign in\"]")
+    error_message_element = (By.CSS_SELECTOR, ".ht-alert__content p")
 
     def __init__(self, webdriver):
         self.webdriver = webdriver
@@ -23,3 +24,8 @@ class LoginPage:
     def sign_in(self):
         sign_in_button = self.webdriver.find_element(*self.sign_in_button)
         sign_in_button.click()
+
+    @property
+    def error_message(self):
+        error = self.webdriver.find_element(*self.error_message_element).text
+        return error
--- tests/step_definitions/test_login_error_handling.py
+++ tests/step_definitions/test_login_error_handling.py
@@ -7,12 +7,13 @@ from selenium.webdriver.common.by import By
 scenarios('../features/login_error_handling.feature')
 
 
-@given("Cucumber.io login page")
+@given("Cucumber.io login page", target_fixture="login_page")
 def display_login_page(browser):
     page = HomePage(browser)
     page.load()
     page.accept_cookies()
     page.login()
+    return LoginPage(browser)
 
 
 @when(
@@ -21,15 +22,12 @@ def display_login_page(browser):
         '(email: "{email_address}", password: "{password}")'
     )
 )
-def login(browser, email_address, password):
-    login_page = LoginPage(browser)
+def login(login_page, email_address, password):
     login_page.send_keys_to_email(email_address)
     login_page.send_keys_to_password(password)
     login_page.sign_in()
 
 
 @then('An error message shows up')
-def validate_error_message(browser):
-    error_message_element = (By.CSS_SELECTOR, ".ht-alert__content p")
-    error_text = browser.find_element(*error_message_element).text
-    assert error_text == "Invalid email or password."
+def validate_error_message(login_page):
+    assert login_page.error_message == "Invalid email or password."
