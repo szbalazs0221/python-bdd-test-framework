de210e6e6b14f4c39ec23ea4ca4502c7f8ec11b2
baszabo <balazs.szabo@ni.com>
2021-03-25 09:49:17 +0100

    added custom command line argument

--- conftest.py
+++ conftest.py
@@ -2,12 +2,26 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture()
-def browser():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(options=options)
+def browser(request):
+    driver = request.config.getoption("--webdriver")
+
+    if driver == 'chrome':
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        browser = webdriver.Chrome(options=options)
+    elif driver == 'firefox':
+        browser = webdriver.Firefox()
+
     yield browser
     browser.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--webdriver",
+        action='store',
+        choices=['chrome', 'firefox'],
+        required=True)
 
 
 def pytest_bdd_step_error(step_func_args):
